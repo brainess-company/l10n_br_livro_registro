acf8e52c73424388b72a8bbd214e470a17e7e56e
BrainessCompany <brainesscompany@gmail.com>
2025-01-07 17:09:17 -0300

    teste

--- __manifest__.py
+++ __manifest__.py
@@ -1,9 +1,9 @@
 {
     "name": "Livro Registro de Entradas e Saídas",
     "summary": "Geração dos relatórios Livro Registro de Entradas (P1/A) e Saídas (P2/A)",
-    "version": "1.0",
-    "author": "Sua Empresa",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
+    "author": "brainess",
     "depends": ["base", "sale", "purchase", "l10n_br_fiscal"],
     "data": [
         "views/livro_registro_views.xml",
