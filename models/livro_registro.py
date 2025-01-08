8a7992a7922278fd7194a4358181c228af8c663c
BrainessCompany <brainesscompany@gmail.com>
2025-01-08 15:10:31 -0300

    a

--- models/livro_registro.py
+++ models/livro_registro.py
@@ -36,7 +36,7 @@ class LivroRegistroRelatorio(models.TransientModel):
 
     def _gerar_pdf(self, documentos):
         # Referenciar o template XML criado
-        report_template = self.env.ref('livro_registro.livro_registro_pdf_template')
+        report_template = self.env.ref('livro_registro.reports_livro_registro_pdf_template')
 
         # Definir os dados a serem passados ao template
         data = {
