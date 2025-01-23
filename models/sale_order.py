ce284c46f56a5f23727ccb69f535064444814102
BrainessCompany <brainesscompany@gmail.com>
2025-01-23 13:00:15 -0300

    atualizando o partner_id para ele mesmo

--- models/sale_order.py
+++ models/sale_order.py
@@ -6,7 +6,11 @@ class SaleOrder(models.Model):
     @api.onchange('partner_id')
     def _onchange_partner_id_update_fiscal_operation(self):
         for order in self:
+            # Reatribuir o cliente a ele mesmo para disparar gatilhos
+            order.partner_id = order.partner_id
+
+            # Atualizar fiscal_operation_id nas linhas
             for line in order.order_line:
                 if line.fiscal_operation_id:
-                    # Atribuir novamente o mesmo valor para disparar gatilhos
+                    # Reatribuir o valor atual para disparar gatilhos
                     line.fiscal_operation_id = line.fiscal_operation_id.id
