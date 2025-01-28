bc9f5f15b1324f9e5ecf6b9b85e3976229c44e70
BrainessCompany <brainesscompany@gmail.com>
2025-01-28 15:44:33 -0300

    teste

--- models/sale_order.py
+++ models/sale_order.py
@@ -7,14 +7,16 @@ class SaleOrder(models.Model):
 
     def update_partner_using_form(self):
         for order in self:
-            # Criar um formulário para a ordem de venda
             order_form = Form(order)
-
-            # Atualizar o parceiro no formulário
             order_form.partner_id = order.partner_id
-
-            # Salvar as mudanças feitas pelo formulário
             updated_order = order_form.save()
-
-            # Opcional: retornar ou fazer algo com o pedido atualizado
             return updated_order
+
+    def write(self, values):
+        # Verificar se o campo 'partner_id' foi alterado
+        if 'partner_id' in values:
+            # Chamar a função quando o 'partner_id' for alterado
+            self.update_partner_using_form()
+
+        # Chamar o método 'write' original para salvar as mudanças
+        return super(SaleOrder, self).write(values)
